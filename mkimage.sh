57c0a9cb508260128e76a7476184257f3871f839
Ocawesome101 <ocawesome101@gmail.com>
2022-09-19 17:16:13 -0400

    fix some issues with kernel config and with mkimage

--- mkimage.sh
+++ mkimage.sh
@@ -19,6 +19,29 @@ blue="\e[94m"
 white="\e[97m"
 res="\e[39;49m"
 
+if [ "$#" = 0 ] || ! [ -e "$1" ]; then
+  printf "usage: $(basename $0) DEVICE\n" 1>&2
+  exit 1
+fi
+
+printf "$red=>$yellow REALLY erase $green$1$yellow? This operation cannot be undone!$white "
+
+while true; do
+  printf "[y/N]: "
+  read;
+  case "$REPLY" in
+    [yY])
+      break
+      ;;
+    [nN])
+      exit 0
+      ;;
+    "")
+      exit 0
+      ;;
+  esac
+done
+
 LINUX_VERSION=${LINUX_VERSION:-"5.14.8"}
 BUSYBOX_VERSION=${BUSYBOX_VERSION:-"1.35.0"}
 
@@ -48,7 +71,7 @@ check () {
       printf "${red}Could not find a required utility$res\n"
       exit 1
     fi
-    export $1=$found
+    export $(echo $1 | sed 's/-/_/g')=$found
     printf "${green}found$white: $yellow$found$res\n"
     shift
   done
@@ -57,8 +80,10 @@ check () {
   printf "\e[${oargn}A$blue=>$white Checking required utilities...$green done$res\e[${oargn}B\e[G"
 }
 
-check syslinux gcc make curl sfdisk bash git patch tar unxz sudo \
+check syslinux gcc-11 make curl sfdisk bash git patch tar unxz sudo \
   strip dd mkfs partprobe
+
+gcc=${gcc_11}
 
 mkdir -p $basedir
 cd $basedir
@@ -68,6 +93,8 @@ printf "$blue=>$white Making sure you have the source code$res\n"
 if ! [ -e "$basedir/$kernel_tarfile" ]; then
   printf "  $blue-$white Downloading source code:$yellow Linux$res\n"
   $curl --progress-bar "$kernel_url" > "$basedir/$kernel_tarfile"
+else
+  printf "  $blue-$white Source tarball present:$yellow Linux$res\n"
 fi
 
 if ! [ -e "$basedir/$kernel_dirname" ]; then
@@ -78,6 +105,8 @@ fi
 if ! [ -e "$basedir/$busybox_tarfile" ]; then
   printf "  $blue-$white Downloading source code:$yellow BusyBox$res\n"
   $curl --progress-bar "$busybox_url" > "$basedir/$busybox_tarfile"
+else
+  printf "  $blue-$white Source tarball present:$yellow BusyBox$res\n"
 fi
 
 if ! [ -e "$basedir/$busybox_dirname" ]; then
@@ -88,6 +117,11 @@ fi
 if ! [ -d "$basedir/muslcrossmake" ]; then
   printf "$blue=>$white Downloading source code:$yellow musl-cross-make$res\n"
   $git clone "$muslcrossmake" "$basedir/muslcrossmake"
+  cd "$basedir/muslcrossmake"
+  $git checkout 0f22991b8d47837ef8dd60a0c43cf40fcf76217a
+  cd -
+else
+  printf "  $blue-$white Source repository present:$yellow Linux$res\n"
 fi
 
 printf "$blue=>$white Copying configuration files\n"
@@ -99,6 +133,13 @@ if ! [ -e "$basedir/$kernel_dirname/arch/x86/boot/bzImage" ]; then
   printf "$blue=>$white Compiling$yellow Linux$res\n"
   cd "$basedir/$kernel_dirname"
   $make -j$(nproc)
+else
+  printf "  $blue-$white Skipping build:$yellow Linux$res\n"
+fi
+
+if ! [ $(echo $PATH | grep /usr/local/bin) ]; then
+  printf "$blue=>$white Adding$yellow /usr/local/bin$white to your$green PATH$res\n"
+  export PATH="$PATH:/usr/local/bin"
 fi
 
 if ! [ $(command -v i486-linux-musl-gcc) ]; then
@@ -107,11 +148,8 @@ if ! [ $(command -v i486-linux-musl-gcc) ]; then
   $make
   printf "$blue=>$white Installing$yellow musl-cross-make$res\n"
   $sudo $make install
-fi
-
-if ! [ $(echo $PATH | grep /usr/local/bin) ]; then
-  printf "$blue=>$white Adding$yellow /usr/local/bin$white to your$green PATH$res\n"
-  export PATH="$PATH:/usr/local/bin"
+else
+  printf "  $blue-$yellow i486-linux-musl-gcc$white found - Skipping build:$yellow musl-cross-make$res\n"
 fi
 
 if ! [ -e "$basedir/$busybox_dirname/busybox" ]; then
@@ -120,31 +158,43 @@ if ! [ -e "$basedir/$busybox_dirname/busybox" ]; then
   printf "$blue=>$white Patching$yellow include/libbb.h$white\n"
   patch -R include/libbb.h <"$confbase/libbb.h.patch"
   $make
+else
+  printf "  $blue-$white Skipping build:$yellow BusyBox$res\n"
 fi
 
 printf "$blue=>$white Assembling final image\n"
 cd "$basedir"
-$dd if=/dev/zero of=486linux.img bs=1M count=128 conv=notrunc,sync oflag=direct
+device="$1"
+printf "  $blue-$red ERASING$yellow $device$red PERMANENTLY IN 5 SECONDS$res\n"
+sleep 5
+$sudo $dd if=/dev/zero of="$device" bs=1M count=128 conv=notrunc,sync oflag=direct status=progress
 
 printf "  $blue-$white Setting up$yellow boot$white partition$res\n"
-device="$($sudo losetup -f)"
-cat $confbase/sfdisk-layout | $sfdisk "$basedir/486linux.img"
-$sudo losetup -P $device 486linux.img
-$sudo $mkfs -t fat -F 12 ${device}p1
-$sudo $mkfs -t ext2 ${device}p2
-$sudo syslinux -i ${device}p1
+cat $confbase/sfdisk-layout | $sudo $sfdisk $device
+
+partprefix=""
+if [ -e "${device}p1" ]; then
+  partprefix="p"
+fi
+
+$sudo $mkfs -t fat -F 12 ${device}${partprefix}1
+$sudo $mkfs -t ext2 ${device}${partprefix}2
+$sudo syslinux -i ${device}${partprefix}1
 $sudo sync
 $sudo $dd if=/usr/lib/syslinux/bios/mbr.bin of=${device} bs=440 count=1 \
   conv=notrunc
-$sudo mount ${device}p1 /mnt
+$sudo mount ${device}${partprefix}1 /mnt
 $sudo cp $confbase/syslinux.cfg /mnt/
 $sudo cp $basedir/$kernel_dirname/arch/x86/boot/bzImage /mnt/linux
 $sudo umount /mnt
 
 printf "  $blue-$white Setting up$yellow root$white partition$res\n"
-$sudo mount ${device}p2 /mnt
+$sudo mount ${device}${partprefix}2 /mnt
 $sudo mkdir /mnt/{bin,dev,proc,sys,root,etc} /mnt/etc/init.d
 $sudo cp $basedir/$busybox_dirname/busybox /mnt/bin/busybox
+$sudo mknod /mnt/dev/tty1 c 4 1
+$sudo mknod /mnt/dev/console c 5 1
+$sudo mknod /mnt/dev/null c 1 3
 cd /mnt/bin
 $sudo ./busybox --install /mnt/bin
 cd -
@@ -155,7 +205,5 @@ $sudo cp $confbase/initrc /mnt/etc/init.d/rc
 $sudo cp $confbase/{passwd,group,welcome} /mnt/etc/
 
 $sudo umount /mnt
-sleep 0.5
-$sudo losetup -d $device
 
-printf "$blue=>$white The finished image is available at $yellow$basedir/486linux.img$res\n"
+printf "$blue=>$white Setup of $yellow$device$white complete.$res\n"
